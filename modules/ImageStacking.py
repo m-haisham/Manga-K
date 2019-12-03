3bb44a98b83b4ed22ecdabdc65423b9ac44e59c9
mHaisham <mHaisham@users.noreply.github.com>
2019-12-03 16:16:38 +0500

    Update ImageStacking.py

--- modules/ImageStacking.py
+++ modules/ImageStacking.py
@@ -151,7 +151,7 @@ def dir_to_img(path, save_path):
     VerticalStack().stack(path, save_path)
 
 
-def dir_to_pdf(path, save_path):
+def dir_to_pdf(path, save_path, verbose = False):
     # get all directory paths
     dirs = sorted(os.listdir(path), key=numericalSort)
 
@@ -176,10 +176,10 @@ def dir_to_pdf(path, save_path):
                 new_img = new_img.convert('RGBA')
 
             # if image mode is RGBA
-            if (new_img.mode == 'RGBA'):
+            if new_img.mode == 'RGBA':
                 # convert image to RGB
-                print(
-                    f'[CONVERT] [{os.path.basename(os.path.normpath(directory)).upper()}] RGBA to RGB')
+                if verbose:
+                    print(f'[CONVERT] [{os.path.basename(os.path.normpath(directory)).upper()}] RGBA to RGB')
 
                 # create RGB image with white background of same size
                 rgb = Image.new('RGB', new_img.size, (255, 255, 255))
