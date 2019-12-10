14e90fc3cd59c88a2768860c1253525f507199b7
mHaisham <mHaisham@users.noreply.github.com>
2019-12-10 21:31:09 +0500

    bug fix
    unneeded key presses registering has been stopped

--- modules/resource/raw/script.py
+++ modules/resource/raw/script.py
@@ -1,19 +1,14 @@
 script = """
 next = document.getElementsByClassName("next")[0];
+
 previous = document.getElementsByClassName("previous")[0];
 
 document.addEventListener("keydown", e => {
-  link = false;
-
   if (e.key === "ArrowLeft") {
-    link = previous.getAttribute("href");
+    document.location = previous.getAttribute("href");
   } else if (e.key === "ArrowRight") {
-    link = next.getAttribute("href");
+    document.location = next.getAttribute("href");
   }
-
-  console.log(`go to ${link} from press ${e.key}`);
-
-  document.location = link;
-  return false;
 });
+
 """
